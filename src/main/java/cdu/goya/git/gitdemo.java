87c20bd78b6fafbb93639718cd094a57c076971e
郑晓楠 <810293864@qq.com>
2022-06-24 00:16:28 +0800

    second commit

--- src/main/java/cdu/goya/git/gitdemo.java
+++ src/main/java/cdu/goya/git/gitdemo.java
@@ -3,5 +3,6 @@ package cdu.goya.git;
 public class gitdemo {
     public static void main(String[] args) {
         System.out.println("hello11111111");
+        System.out.println("hello2222222222222");
     }
 }
