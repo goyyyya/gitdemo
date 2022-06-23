929c7a6012665c11e7d976a91faf0c093c207981
郑晓楠 <810293864@qq.com>
2022-06-24 00:54:44 +0800

    hot-fix commit1

--- src/main/java/cdu/goya/git/gitdemo.java
+++ src/main/java/cdu/goya/git/gitdemo.java
@@ -4,5 +4,6 @@ public class gitdemo {
     public static void main(String[] args) {
         System.out.println("hello11111111");
         System.out.println("hello2222222222222");
+        System.out.println("hell33333333333");
     }
 }
